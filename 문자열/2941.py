a525b99d641e966ae89a689fb7a0401fe4921ac8
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-02-17 23:53:23 +0900

    Update 2941.py

--- 문자열/2941.py
+++ 문자열/2941.py
@@ -3,35 +3,56 @@
 str1 = str(input())
 count = 0
 
-for i in range(len(str1) - 2):  # 2글자짜리
-    if str1[i] == 'c' and str1[i+1] == '=':
-        count += 1
-        i += 2
-    if str1[i] == 'c' and str1[i+1] == '-':
-        count += 1
-        i += 2
-    if str1[i] == 'd' and str1[i+1] == '-':
-        count += 1
-        i += 2
-    if str1[i] == 'l' and str1[i+1] == 'j':
-        count += 1
-        i += 2
-    if str1[i] == 'n' and str1[i+1] == 'j':
-        count += 1
-        i += 2
-    if str1[i] == 's' and str1[i+1] == '=':
-        count += 1
-        i += 2
-    if str1[i] == 'z' and str1[i+1] == '=':
-        count += 1
-        i += 2
-    if str1[i] == 'd' and str1[i + 1] == 'z' and str1[i + 2] == '=':
-        count += 1
-        i += 3
-    else:
+for i in range(0, len(str1)):
+    if str1[i:i+2] == 'c=':
         count += 1
 
+str1 = str1.replace("c=", "")  # 해당 문자열 삭제
 
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 'c-':
+        count += 1
+
+str1 = str1.replace("c-", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 3):
+    if str1[i:i+3] == 'dz=':
+        count += 1
+
+str1 = str1.replace("dz=", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 'd-':
+        count += 1
+
+str1 = str1.replace("d-", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 'lj':
+        count += 1
+
+str1 = str1.replace("lj", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 'nj':
+        count += 1
+
+str1 = str1.replace("nj", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 's=':
+        count += 1
+
+str1 = str1.replace("s=", "")  # 해당 문자열 삭제
+
+for i in range(0, len(str1) - 2):
+    if str1[i:i+2] == 'z=':
+        count += 1
+
+str1 = str1.replace("z=", "")  # 해당 문자열 삭제
+
+print(len(str1))
+count = count + len(str1)  # 남은 1개짜리 알파벳 개수 모두 더해주기
 
 print(count)
 
