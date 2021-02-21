4a58dc4908c2d35a0fdeddfbcdeedd09ea726f67
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-02-22 00:25:24 +0900

    group word checkers

--- 문자열/1316.py
+++ 문자열/1316.py
@@ -6,8 +6,11 @@ count = 0
 for i in range(N):
     word = str(input())
     lst = [k for k in str(word)]  # 문자열을 리스트 형식으로 바꾸기
-    for j in range(len(word) - 1):
-        if word[j] != word[j+1] and word[j] != " ":
-            lst.insert(j + 1, " ")
+    j = 0
+    while j < len(lst):
+        while lst[j] == lst[j + 1]:
+            j += 1
+        lst.insert(j + 1, " ")
 
 print(lst)
+
