64e7c719309989240ac7190deb3e461f643c8a1f
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-02-24 22:04:09 +0900

    find break-even point, if exists
    Since A, B, C is natural number under 2.1 billion (21억), time limit exceeded error occurs when loop is used

---
+++ 기본 수학 1/1712.py
@@ -0,0 +1,9 @@
+A, B, C = map(int, input().split())
+
+if C - B == 0:  # avoid dividebyzero
+    print(-1)
+elif A / (C - B) <= 0:  # avoid when there is no break-even point (손익분기점이 없을 경우) 
+    print(-1)
+else:  # when break-even point exists
+    N = int(A / (C - B))
+    print(N + 1)
